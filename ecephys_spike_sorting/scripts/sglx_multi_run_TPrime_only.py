eb409ed945eab79c669707e90e4401e04e98e981
DT <31556990+MrbioDT@users.noreply.github.com>
2026-06-03 09:16:05 -0700

    nidq.times.adj.npy for 0711！ done

--- ecephys_spike_sorting/scripts/sglx_multi_run_TPrime_only.py
+++ ecephys_spike_sorting/scripts/sglx_multi_run_TPrime_only.py
@@ -88,7 +88,7 @@ npx_directory = r"Y:\SGL_DATA_drive"
 # ]
 
 run_specs = [
-						['wmm001_250709_rec_d13', '0,1', '0,0', '0', ['thalamus'] ]
+						['wmm001_250711_rec_d14', '0,2', '0,0', '0', ['thalamus'] ]
 ]
 
 # ------------------
@@ -100,7 +100,7 @@ run_specs = [
 # catGT_dest = r'D:\SC048_out'
 # catGT_dest = r'C:\SGL_DATA\For_Katie\wmm001_250625_rec_d9_g0'
 # catGT_dest = r'Y:\SGL_DATA_drive\wmm001_250702_rec_d10'
-catGT_dest = r'Y:\SGL_DATA_drive\wmm001_250709_rec_d13'
+catGT_dest = r'Y:\SGL_DATA_drive\wmm001_250711_rec_d14'
 
 # ------------
 # CatGT params
